73fd54edac8be255783c8c8e748765e0e410a090
omegacat <anisimov.develop@gmail.com>
2019-10-02 23:36:21 +0300

    Added '/geocode/ method to YandexMapsApi;

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/users', require('./users'));
 router.use('/maps', require('./maps'));
+router.use('/ya-maps', require('./ya-maps'));
 
 module.exports = router;
